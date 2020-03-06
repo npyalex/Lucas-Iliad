d64fad948206555f6f0b66d45aa2ee1afe1bba25
Nick <np.alexander@gmail.com>
2020-03-06 16:39:09 -0500

    Backup Scripts
    2020-03-06

---
+++ Assets/DataFiles/Scripts/ClickListener.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickListener : MonoBehaviour
+{
+    public GameObject videoObject, audioObject, offButton, onButton, stopButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Button Clicked");
+        StopAllAudio();
+        videoObject.SetActive(true);
+        audioObject.SetActive(true);
+        offButton.SetActive(false);
+        onButton.SetActive(true);
+    }
+
+    void Awake()
+    {
+        allAudioSources = Resources.FindObjectsOfTypeAll(typeof(AudioSource)) as AudioSource[];
+    }
+
+    private AudioSource[] allAudioSources;
+    void StopAllAudio()
+    {
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
+        }
+    }
+}
---
+++ Assets/DataFiles/Scripts/StopClickListener.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopClickListener : MonoBehaviour
+{
+    public GameObject videoObject, audioObject, playButton, stopButton;
+    private AudioSource[] allAudioSources;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //stopButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnMouseDown()
+    {
+        StopAllAudio();
+        Debug.Log("Stop Button clicked");
+        videoObject.SetActive(false);
+        audioObject.SetActive(false);
+        playButton.SetActive(true);
+        stopButton.SetActive(false);
+    }
+
+    void Awake()
+    {
+        allAudioSources = FindObjectsOfTypeAll(typeof(AudioSource)) as AudioSource[];
+    }
+
+    void StopAllAudio()
+    {
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
+        }
+    }
+}
